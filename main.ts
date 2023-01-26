4cfe829227c9694da7f15b54a19c374391118617
Nguyen Ngoc Chi <chinn.com.vn@gmail.com>
2023-01-26 12:56:24 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+led.plot(0, 0)
+let iCommon = 0
+if (false) {
+    radio.setGroup(1)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+} else if (led.point(0, 0)) {
+    led.unplot(0, 0)
+    led.toggle(0, 0)
+}
+basic.forever(function () {
+	
+})
