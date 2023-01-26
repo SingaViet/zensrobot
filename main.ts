910fa4a5f795fc21c302a918108daeede6ebaa49
Nguyen Ngoc Chi <chinn.com.vn@gmail.com>
2023-01-26 13:03:08 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,14 @@ let iCommon = 0
 if (false) {
     radio.setGroup(1)
     music.playTone(262, music.beat(BeatFraction.Whole))
+    radio.sendNumber(0)
+    led.plotBarGraph(
+    0,
+    0
+    )
 } else if (led.point(0, 0)) {
     led.unplot(0, 0)
+    music.playTone(262, music.beat(BeatFraction.Whole))
     led.toggle(0, 0)
 }
 basic.forever(function () {
